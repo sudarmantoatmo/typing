17c2dba2d1113080ae81e6e5b9434ae464e5337c
sudarmanto <sudarmanto.atmo@gmail.com>
2023-01-19 20:26:17 +0700

    53. Styling by Comparison

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { lorem } from 'faker';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   randomText = lorem.sentence();
@@ -14,5 +14,11 @@ export class AppComponent {
     this.enteredText = value;
   }
 
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
 
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
 }
